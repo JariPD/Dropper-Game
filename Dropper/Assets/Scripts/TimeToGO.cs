eabe72a863b03442f5f2c902267540a1edd975b7
Jari <dijkjari@gmail.com>
2021-03-17 10:03:14 +0100

    next scene loader

--- Dropper/Assets/Scripts/TimeToGO.cs
+++ Dropper/Assets/Scripts/TimeToGO.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TimeToGO : MonoBehaviour
 {
@@ -14,7 +15,7 @@ public class TimeToGO : MonoBehaviour
 
     private Collision col;
 
-    float timeLeft = 45.0f;
+    float timeLeft = 3.0f;
 
     private FirstPersonController FPC;
 
@@ -50,7 +51,8 @@ public class TimeToGO : MonoBehaviour
             timeLeft = 45f;
             timeText.SetActive(true);
 
-            col.TPPlayerToSpawn();
+            //col.TPPlayerToSpawn();
+            SceneManager.LoadScene("Niels");
         }
     }
 
